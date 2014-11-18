71c7f7d0eba584d78f2df052db85d73fec4f5911
Eric Iacutone <eric.iacutone@gmail.com>
2014-11-17 22:09:49 -0500

    Reconfigure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   
+  devise_for :trucks
   namespace :api do
     namespace :v1 do
-      devise_for :trucks do
+      devise_scope :truck do
         post 'registrations' => 'registrations#create', :as => 'register'
         post 'sessions' => 'sessions#create', :as => 'login'
         delete 'sessions' => 'sessions#destroy', :as => 'logout'
