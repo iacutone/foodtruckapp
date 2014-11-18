d3eeb45b977118c63d2fc1b6cc46fae4af2e352f
Eric Iacutone <eric.iacutone@gmail.com>
2014-11-17 21:50:18 -0500

    remove has_secure_password

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,5 +1,6 @@
 class Truck
   include Mongoid::Document
+  include Mongoid::Timestamps
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -33,9 +34,7 @@ class Truck
   # field :failed_attempts, type: Integer, default: 0 # Only if lock strategy is :failed_attempts
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
-  include Mongoid::Timestamps
   
-  has_secure_password
   
   has_many :locations
   
