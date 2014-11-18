9a3672e42ebe36bd5e81075b4add34304756938c
Eric Iacutone <eric.iacutone@gmail.com>
2014-11-17 23:49:36 -0500

    Feel the good vibes

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,9 +1,8 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :verify_authenticity_token,
-                     :if => Proc.new { |c| c.request.format == 'application/json' }
+  skip_before_filter :verify_authenticity_token
 
-  respond_to :json
-
+  # respond_to :json
+  
   def create
     puts resource
     build_resource
@@ -13,8 +12,8 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
       render :status => 200,
            :json => { :success => true,
                       :info => t("devise.registrations.signed_up"),
-                      :data => { :user => resource,
-                                 :auth_token => current_user.authentication_token } }
+                      :data => { :truck => resource,
+                                 :auth_token => current_truck.authentication_token } }
     else
       render :status => :unprocessable_entity,
              :json => { :success => false,
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
+  root 'trucks#new'
+  resource :trucks
   
-  devise_for :trucks, :controllers => { registrations: 'api/v1/registrations' }
+  devise_for :trucks #, :controllers => { registrations: 'api/v1/registrations' }
   namespace :api do
     namespace :v1 do
       devise_scope :truck do
-        post 'registrations', to:'registrations#create'
+        post 'registrations' => 'registrations#create', :as => 'register'
         post 'sessions' => 'sessions#create', :as => 'login'
         delete 'sessions' => 'sessions#destroy', :as => 'logout'
       end
     end
   end
-  root 'trucks#new'
+  
 end
