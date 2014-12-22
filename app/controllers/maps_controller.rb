1de3e0b0b3b766bee2afcf2df1a8a8a01caaf75c
AlexGorski <alex.gorski@gmail.com>
2014-12-21 19:50:50 -0500

    more partial work

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,36 +1,32 @@
 class MapsController < ApplicationController
   include ActionController::Live
 
-  respond_to :json
   #each truck on the client side will subscribe to that truck's channel via truck.id
   #each ios truck will push its location to a route, which will publish lat long to that truck's channel
   #figure out if Pubs stay in memory or if they are killed after subscribers read
   def index
-    @maps = Map.all
-    respond_with(@maps)
+
   end
 
   def locations_stream
     response.headers['Content-Type'] = 'text/event-stream'
-      redis = Redis.new
-      redis.subscribe('namespaced:stream') do |on|
-        on.message do |event, data|
-          response.stream.write("data:#{ data }\n\n")
-        end
-      end
-      ensure
-        response.stream.close
+    redis = Redis.new
+    redis.subscribe('stream') do |on|
+      on.message do |event, data|
+        response.stream.write("data:#{ data }\n\n")
       end
     end
+    ensure
+      response.stream.close
   end
 
 
   private
-    def set_map
-      @map = Map.find(params[:id])
-    end
+  def set_map
+    @map = Map.find(params[:id])
+  end
 
-    def map_params
-      params[:map]
-    end
+  def map_params
+    params[:map]
+  end
 end
