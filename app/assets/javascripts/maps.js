13d279c6bf2acbfb2f95a095debb8db28c55dcdc
AlexGorski <alex.gorski@gmail.com>
2015-02-09 20:06:21 -0500

    Add javascript to set markers on Google map. Centered on Apple

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -3,23 +3,42 @@
 // All this logic will automatically be available in application.js.
 $(document).ready(function(){
   var mapOptions = {
-      center: { lat: -34.397, lng: 150.644},
-      zoom: 8
+      center: { lat: 37.333, lng: -122.031},
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
-    //var map = new google.maps.Map(document.getElementById('map-canvas'),
-       // mapOptions);
-
   
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
   var source = new EventSource('/locations_stream');
     source.addEventListener('message', function(e) {
-        console.log("Received "+e.data);
-        updateItemsPage(e.data);
+        setMarker(e.data); //should call resolveMarker once truck_id available via the stream
     }, false);
-// need to insert marker instead of <li> and add conditional that places new markers based on
-// a new truck_id data point, and or replaces coordinates for an existing one
-  function updateItemsPage(event) {
-    var item = $('<li>').text(event);
-    $('#map-canvas').prepend(item);
+
+  function resolveMarker(event) {
+    // see if map has a marker via truck_id
+    // if marker is not on map, call setMarker
+    // if marker is on map call updateMarker
+  }
+
+  function setMarker(event) {
+    data = JSON.parse(event);
+
+    var truck_name = data.truck_name;
+    var truck_id = "";
+    var markerPosition = new google.maps.LatLng(data.location.latitude,data.location.longitude);
+
+    var marker = new google.maps.Marker({
+      position:markerPosition,
+      name: truck_name,
+      id: truck_id
+    });
+    marker.setMap(map);
+  }
+
+  function updateMarker(event) {
+    // if marker's location is significantly different than stream data, update marker
+    // otherwise do nothing
   }
 })
     
