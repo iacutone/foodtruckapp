667616cc701fbbb754ecb71b70c75894eee0a4eb
AlexGorski <alex.gorski@gmail.com>
2015-02-19 19:07:46 -0500

    Updates for user module

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -2,30 +2,60 @@ class Api::V1::RegistrationsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    truck = Truck.new(params['truck'].permit(:email, :password, :name))
-    if truck.save
-      sign_in(truck)
-      render(
-              status: 200,
-              json: {
-                success: true,
-                data: {
-                  truck: truck,
-                  auth_token: current_truck.token,
-                  name: truck.name,
-                  id: truck.id.to_s
+    if params['truck']
+      truck = Truck.new(params['truck'].permit(:email, :password, :name))
+      if truck.save
+        sign_in(truck)
+        render(
+                status: 200,
+                json: {
+                  success: true,
+                  data: {
+                    truck: truck,
+                    auth_token: current_truck.token,
+                    name: truck.name,
+                    id: truck.id.to_s,
+                    kind: "truck"
+                  }
                 }
-              }
-            )
-    else
-      render(
-              status: :unprocessable_entity,
-              json: {
-                success: false,
-                info: truck.errors.full_messages,
-                data: {}
-              }
-            )
+              )
+      else
+        render(
+                status: :unprocessable_entity,
+                json: {
+                  success: false,
+                  info: truck.errors.full_messages,
+                  data: {}
+                }
+              )
+      end
+    elsif params['user']
+      user = User.new(params['user'].permit(:email, :password, :name))
+      if user.save
+        sign_in(user)
+        render(
+                status: 200,
+                json: {
+                  success: true,
+                  data: {
+                    user: user,
+                    auth_token: current_user.token,
+                    name: user.name,
+                    id: user.id.to_s,
+                    kind: "user"
+                  }
+                }
+              )
+      else
+        render(
+                status: :unprocessable_entity,
+                json: {
+                  success: false,
+                  info: user.errors.full_messages,
+                  data: {}
+                }
+              )
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
-  end
   
-  def sign_in(truck)
-    cookies.permanent[:token] = truck.token
-    self.current_truck = truck
+  def sign_in(thing)
+    cookies.permanent[:token] = thing.token
+    if thing.class.to_s == "Truck"
+      self.current_truck = thing
+    else 
+      self.current_user = thing
+    end
   end
 
   def current_truck=(truck)
@@ -23,9 +21,18 @@ class ApplicationController < ActionController::Base
   def current_truck
     @current_truck ||= Truck.find_by_token(cookies[:token])
   end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_token(cookies[:token])
+  end
   
   def sign_out
     self.current_truck = nil
+    self.current_user = nil
     cookies.delete(:token)
   end
 end
